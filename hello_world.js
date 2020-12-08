13fe95931a6f1a00a93e5c461eab0f332bf96287
Daniel Goldberg <d.goldberg@wustl.edu>
2020-12-08 17:28:39 -0500

    local

--- hello_world.js
+++ hello_world.js
@@ -1,13 +1,14 @@
-const express = require("http"),
-    fs = require("fs");
+const http = require("http"),
+    fs = require("fs"); //file server
+const express = require('express')
 const app = express();
 const port = 3456;
-const file = "client.html";
+/*const file = "client.html";
 const cssfile = "client.css";
 
-const server = express.createServer(function (req, res) {
+/*const server = http.createServer(function (req, res) {
     res.send('Hello World!');
-})
+})*/
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
