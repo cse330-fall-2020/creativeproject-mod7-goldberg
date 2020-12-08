3fbd2ad5f7040cf2ed63a09fd357a69ae4bc5416
Daniel Goldberg <d.goldberg@wustl.edu>
2020-12-08 15:58:38 -0500

    stage

--- hello_world.js
+++ hello_world.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3456
+const express = require("http"),
+    fs = require("fs");
+const app = express();
+const port = 3456;
+const file = "client.html";
+const cssfile = "client.css";
+
+const server = express.createServer(function (req, res) {
+    res.send('Hello World!');
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
